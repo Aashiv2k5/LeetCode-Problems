aa904db3e07168bf84aabb0bbd65cacdb45d1540
Aashiv <148091917+Aashivdev@users.noreply.github.com>
2026-05-02 14:55:22 +0530

    Time: 2 ms (100%), Space: 47 MB (9.99%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        int i = s.length()-1;
+        int sum=0;
+        while(i>=0){
+          if(s.charAt(i)=='I'){
+            sum+=1;
+          }
+          else if(s.charAt(i)=='V'){
+            if(i>0 && s.charAt(i-1)=='I'){
+                sum+=4;
+                i--;
+            }else{
+                sum+=5;
+            }
+          }
+          else if(s.charAt(i)=='X'){
+            if(i>0 && s.charAt(i-1)=='I'){
+                sum+=9;
+                 i--;
+            }
+            else{
+                sum+=10;
+            }
+          }
+          else if(s.charAt(i)=='L'){
+           if(i>0 && s.charAt(i-1)=='X'){
+                sum+=40;
+                 i--;
+            }
+            else {
+                sum+=50;
+            }
+          }
+           else if(s.charAt(i)=='C'){
+            if(i>0 && s.charAt(i-1)=='X'){
+                sum+=90;
+                 i--;
+            }
+            else {
+                sum+=100;
+            }
+          }
+           else if(s.charAt(i)=='D'){
+            if(i>0 && s.charAt(i-1)=='C'){
+                sum+=400;
+                 i--;
+            }
+            else {
+                sum+=500;
+            }
+          }
+           else if(s.charAt(i)=='M'){
+            if(i>0 && s.charAt(i-1)=='C'){
+                sum+=900;
+                 i--;
+            }
+            else {
+                sum+=1000;
+            }
+          }
+          i--;
+        }
+        return sum;
+        
+    }
+}
